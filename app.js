c46e493c21a903e96c3ff8f32e209f81f76db9d2
juandinetUPC <juandinet@unicesar.edu.co>
2022-03-30 09:50:13 -0500

    primer commit

---
+++ app.js
@@ -0,0 +1,21 @@
+/* Aplicacion que me guarde los datos en una tabla*/
+//declaracion de variables
+var nombre;
+var apellido;
+var telefono;
+var email;
+var direccion;
+var fecha;
+var nota;
+var tabla = document.getElementById("tabla");
+var guardar = document.getElementById("guardar");
+var contador = 1;
+//Al hacer click al boton, ejecuta la siguiente accion
+guardar.addEventListener("click",function(){
+
+    console.log("se ha dado click al boton");
+
+
+
+})
+
